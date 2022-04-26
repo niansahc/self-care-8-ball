bdbb4c417228e6e0c8d2f0e38c74182c8db42616
niansahc <97317555+niansahc@users.noreply.github.com>
2022-04-26 12:41:36 -0400

    Reloading to proper repository

---
+++ src/App.js
@@ -0,0 +1,96 @@
+import React, {useState} from 'react';
+import './App.css';
+//import { ReactComponent as Logo } from '.selfcare8.svg'; 
+
+function App() {
+
+const [userInput, setUserInput] = useState('');
+const [randomIndex, setRandomIndex] = useState('');
+const [error, setError] = useState('');
+
+const handleChange = (event) => {
+  event.preventDefault();
+  console.log(event.target.value);
+  setUserInput(event.target.value);
+}
+
+const handleClick = () => {
+
+  if(userInput)
+  {
+    setError('')
+  setRandomIndex(Math.round(Math.random() * 20));
+  setUserInput('');
+  }
+  else{
+    setError('Type your feelings above!')
+  }
+  console.log(randomIndex);
+}
+
+const possibleAnswers = [
+  'Have some water',
+  'Text a friend',
+  'Take a deep breath',
+  'Have you eaten something?',
+  'Remember you are enough',
+  'How does a nap sound?',
+  'Move your body',
+  'Your fave music, STAT!',
+  'Treat yourself',
+  'Ask for help',
+  'Help someone else',
+  'Enjoy some nature',
+  'Definitely cat videos',
+  '15 minutes of worry time',
+  'Take a bath or shower',
+  'Try mindfulness',
+  'Try something new',
+  'Hug yourself, a pet, a friend',
+  'Blanket time',
+  'Hydration, my friend'
+  'Read/watch/listen- enjoy media!'
+];
+const answer = possibleAnswers[randomIndex];
+  return (
+    <div className="App">
+      <h1>How are you feeling?</h1>
+      <p className="info">I'll tell you how you can be kind to yourself!</p>
+      <input type="text" className="question" value={userInput} onChange={handleChange} />
+      <div className="eight-ball">
+      <div class="cow">
+  <div class="head">
+    <div class="ear left"></div>
+    <div class="ear right"></div>
+    <div class="face"></div>
+    <div class="nose"></div>
+    <div class="grass"></div>
+  </div>
+  <div class="body">
+    <div class="spot"></div>
+    <div class="spot"></div>
+    <div class="spot"></div>
+    <div class="spot"></div>
+    <div class="spot"></div>
+    <div class="spot"></div>
+    <div class="spot"></div>
+    <div class="spot"></div>
+    <div class="spot"></div>
+    <div class="spot"></div>
+  </div>
+  <div class="leg front left"></div>
+  <div class="leg front right"></div>
+  <div class="leg back left"></div>
+  <div class="leg back right"></div>
+  <div class="tail"></div>
+</div>
+        <div className="content" onClick={handleClick}>
+        {error ? <p className="error">{error}</p> :
+          answer ? <p className="answer">{answer}</p> : <p className="eight">8</p>}
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+export default App;
